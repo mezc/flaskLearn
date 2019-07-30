d5f62aba0704a71f346ab91e9a9e8ea69403cbbd
Czhang <mezc@live.cn>
2019-07-30 15:44:42 +0800

    sql_cypher finish

--- app.py
+++ app.py
@@ -1,35 +1,20 @@
-from flask import Flask
-# 从flask引入request实例
-from flask import request, url_for
+import pymysql
+# connect
+db = pymysql.connect("localhost", "root", "666666", "test")
+cursor = db.cursor(cursor=pymysql.cursors.DictCursor)
+# cursor = db.cursor()
+while True:
+  name = input("please input:")
+  sql = "select * from students"
+  # insert_sql = "insert into students (name) values ('wanger')"
+  insert_sql = "insert into students (name) values ('{}')".format(name)
+  cursor.execute(insert_sql)
 
-app = Flask(__name__)
+  db.commit()
 
+  cursor.execute(sql)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-# 换请求方式
-@app.route('/user', methods=['POST'])
-def hello_user():
-    return 'Hello User!'
-
-# 传递参数
-@app.route('/user/<id>')
-def hello_id(id):
-    return 'Hello id:{}'.format(id)
-
-@app.route('/query_user')
-def query_id():
-    id = request.args.get('id')
-    # 访问：http://127.0.0.1:5000/query_user?id=12345
-    return 'query_user id:{}'.format(id)
-
-# 方向路由：通过视图函数，反找出url地址
-@app.route('/query_url')
-def query_url():
-    # 访问：http://127.0.0.1:5000/query_url
-    return 'query_url:{}'.format(url_for('query_id'))
-
-if __name__ == '__main__':
-    app.run()
+  # cursor.close()
+  # db.close()
+  data = cursor.fetchall()
+  print(data)
