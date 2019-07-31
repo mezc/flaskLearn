fe1b578a40ce322ae521ff72a9288dd0bd692622
Czhang <mezc@live.cn>
2019-07-31 14:59:58 +0800

    to do first finish

--- to-do/app/__init__.py
+++ to-do/app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
-
+from flask_mongoengine import MongoEngine
 app = Flask(__name__)
+
+app.config.from_object('config')
+
+db = MongoEngine(app) #实例化mongoengine
 
 from app import views, models
---
+++ to-do/app/models.py
@@ -0,0 +1,7 @@
+from app import db
+import datetime
+
+class Todo(db.Document):
+  content = db.StringField()
+  time = db.DateTimeField(default = datetime.datetime.now())
+  status = db.IntField(default=0)
