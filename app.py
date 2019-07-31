11e4960a6d6d8dc81b4fad628c1b0a2046af36f8
Czhang <mezc@live.cn>
2019-07-31 19:53:05 +0800

    ..

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_mongoengine import MongoEngine
+
+app = Flask(__name__)
+
+app.config['MONGODB_SETTINGS'] = {'db': 'jike'}
+
+db = MongoEngine(app) 
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World'
+
+if __name__ == "__main__":
+    app.run()
--- to-do/app/models.py
+++ to-do/app/models.py
@@ -1,5 +1,6 @@
 from app import db
 import datetime
+from flask_mongoengine.wtf import model_form
 
 class Todo(db.Document):
   content = db.StringField()
