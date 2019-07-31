b50693bfa79009f8fed0fd257ec3b1aeba39f2e7
Czhang <mezc@live.cn>
2019-07-31 14:12:48 +0800

    flask mongo

--- manage.py
+++ manage.py
@@ -5,12 +5,15 @@ manager = Manager(app)
 
 @manager.command
 def save():
-  user = User('jike', 'jike@jikexueyuan.com')
+  # user = User('jike', 'jike@jikexueyuan.com')
+  # user.save()
+  user = User(name="jike", email="jike@com")
   user.save()
 
 @manager.command
 def query_users():
-  users = User.query_users()
+  # users = User.query_users()
+  users = User.objects.all()
   for user in users:
     print(user)
 
--- modules.py
+++ modules.py
@@ -1,33 +1,41 @@
-import pymongo
+# import pymongo
 
-MONGO_URL = '127.0.0.1'
-MONGO_DB = "jikexueyuan" #数据库名称
-MONGO_TABLE = "user_collection" #表单名称
+# MONGO_URL = '127.0.0.1'
+# MONGO_DB = "jikexueyuan" #数据库名称
+# MONGO_TABLE = "user_collection" #表单名称
 
-def get_collection():
-  client = pymongo.MongoClient(MONGO_URL)
-  db = client[MONGO_DB]
-  user_collection = db[MONGO_TABLE]
-  return user_collection
+# def get_collection():
+#   client = pymongo.MongoClient(MONGO_URL)
+#   db = client[MONGO_DB]
+#   user_collection = db[MONGO_TABLE]
+#   return user_collection
 
-class User():
-  def __init__(self, name, email):
-    self.name = name
-    self.email = email
+# class User():
+#   def __init__(self, name, email):
+#     self.name = name
+#     self.email = email
 
-  def save(self):
-    user_info = {
-      "name": self.name,
-      "email": self.email
-    }
+#   def save(self):
+#     user_info = {
+#       "name": self.name,
+#       "email": self.email
+#     }
 
-    collection = get_collection()
-    id  = collection.insert(user_info)
-    print(id)
-    
-  @staticmethod
-  def query_users():
-    users = get_collection().find()
-    return users
+#     collection = get_collection()
+#     id  = collection.insert(user_info)
+#     print(id)
+
+#   @staticmethod
+#   def query_users():
+#     users = get_collection().find()
+#     return users
 
 
+from app import db
+
+class User(db.Document):
+  name = db.StringField()
+  email = db.StringField()
+
+  def __str__(self):
+    return "name:{}-email:{}".format(self.name, self.email)
